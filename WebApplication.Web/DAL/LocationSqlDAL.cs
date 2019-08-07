d42daa75c5a274b1aa4169daab5ff1b85a7a8c8d
Jack Bird <jackbird2@gmail.com>
2019-08-06 21:48:29 -0400

    updated DAO tables and added location model

---
+++ WebApplication.Web/DAL/LocationSqlDAL.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication.Web.Models;
+
+namespace WebApplication.Web.DAL
+{
+    public class LocationSqlDAL
+    {
+        private readonly string connectionString;
+
+        public LocationSqlDAL(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+
+        public bool CreateLocation(Models.Location location)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    SqlCommand command = new SqlCommand(@"INSERT INTO Locations (location_Title, location_Description) VALUES(@title, @description);", connection);
+
+
+                    command.Parameters.AddWithValue("@title", location.Title);
+                    command.Parameters.AddWithValue("@description", location.Description);
+
+
+
+                    command.ExecuteNonQuery();
+
+                    if (location.Title == null || location.Description == null)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }
+            }
+            catch (SqlException E)
+            {
+                Console.Write(E);
+                throw;
+            }
+
+        }
+    }
+}
---
+++ WebApplication.Web/Models/Location.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Web.Models
+{
+    public class Location
+    {
+        /// <summary>
+        /// The Location's id.
+        /// </summary>
+        [Required]
+        public int? LocationId { get; set; }
+
+        /// <summary>
+        /// The job's title.
+        /// </summary>
+        [Required]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The job's title.
+        /// </summary>
+        [Required]
+        public string Description { get; set; }
+    }
+}
