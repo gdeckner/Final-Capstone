12849f95683f87f6d96fbaaac7985002c6e5471c
Georg Deckner <georgdeckner@gmail.com>
2019-08-15 07:48:30 -0400

    script tweak

--- TestDataStart.sql
+++ TestDataStart.sql
@@ -30,26 +30,26 @@ Insert into userJob (userID,job_Id) values (07,01),(07,02),(08,03),(09,04),(10,1
 
 insert into Tasks (job_Id,project_Task_Title) values (01,'Fujitsu POS'), (01,'SSE Mobile'),(01,'SHS')
 ,(02,'Payments'),(02,'Shipping'),(02,'Marketing'),(03,'Customer Support Applications'),(03,'Website Team'),(04,'Mentoring'),(04,'New Application Development')
-insert into Hours (dateLogged,dateWorked,task_Title,timeInHours,userID,description,taskId) values
-('07-07-2019','07-01-2019','Fujitsu POS',13,07,'Tested version 11.0 of pos',01),
-('07-10-2019','07-11-2019','Fujitsu POS',32,07,'Tested version 12.0 of pos',01),
-('08-15-2019','08-12-2019','Fujitsu POS',07,07,'Tested version 13.0 of pos',01),
-('08-14-2019','08-14-2019','SSE Mobile',13,07,'Manually tested new app',01),
+insert into Hours (dateLogged,dateWorked,task_Title,timeInHours,userID,description,taskId,location) values
+('07-07-2019','07-01-2019','Fujitsu POS',13,07,'Tested version 11.0 of pos',01,'Rev1 Building'),
+('07-10-2019','07-11-2019','Fujitsu POS',32,07,'Tested version 12.0 of pos',01.'Rev1 Building'),
+('08-15-2019','08-12-2019','Fujitsu POS',07,07,'Tested version 13.0 of pos',01,'Rev1 Building'),
+('08-14-2019','08-14-2019','SSE Mobile',13,07,'Manually tested new app',01,'Home'),
 
-('06-07-2019','06-01-2019','Customer Support Applications',7,08,'worked  mobile support',3),
-('06-16-2019','06-15-2019','Website Team',45,08,'Front end development',3),
-('08-02-2019','08-01-2019','Customer Support Applications',2,08,'worked  mobile support',3),
-('08-14-2019','08-14-2019','Website Team',0,08,'Front end development',3),
+('06-07-2019','06-01-2019','Customer Support Applications',7,08,'worked  mobile support',3,'Las Vegas HQ'),
+('06-16-2019','06-15-2019','Website Team',45,08,'Front end development',3,'Home'),
+('08-02-2019','08-01-2019','Customer Support Applications',2,08,'worked  mobile support',3,'Las Vegas HQ'),
+('08-14-2019','08-14-2019','Website Team',0,08,'Front end development',3,'Home'),
 
-('07-07-2019','07-01-2019','Fujitsu POS',13,09,'Tested version 11.0 of pos',01),
-('07-07-2019','07-01-2019','Fujitsu POS',13,09,'Tested version 11.0 of pos',01),
-('07-07-2019','07-01-2019','Fujitsu POS',13,09,'Tested version 11.0 of pos',01),
-('07-07-2019','07-01-2019','Fujitsu POS',13,09,'Tested version 11.0 of pos',01),
+('07-07-2019','07-01-2019','Fujitsu POS',13,09,'Tested version 11.0 of pos',01,'Home'),
+('07-08-2019','07-02-2019','Fujitsu POS',13,09,'Tested version 11.0 of pos',01,'Home'),
+('07-09-2019','07-03-2019','Fujitsu POS',13,09,'Tested version 11.0 of pos',01,'Home'),
+('07-10-2019','07-04-2019','Fujitsu POS',13,09,'Tested version 11.0 of pos',01,'Home'),
 
-('07-07-2019','07-01-2019','Fujitsu POS',13,10,'Tested version 11.0 of pos',01),
-('07-07-2019','07-01-2019','Fujitsu POS',13,10,'Tested version 11.0 of pos',01),
-('07-07-2019','07-01-2019','Fujitsu POS',13,10,'Tested version 11.0 of pos',01),
-('07-07-2019','07-01-2019','Fujitsu POS',13,10,'Tested version 11.0 of pos',01)
+('07-28-2019','07-28-2019','Fujitsu POS',13,10,'Tested version 11.0 of pos',01,'Home'),
+('07-29-2019','07-29-2019','Fujitsu POS',13,10,'Tested version 11.0 of pos',01,'CA Office'),
+('07-30-2019','07-30-2019','Fujitsu POS',13,10,'Tested version 11.0 of pos',01,'CA Office'),
+('08-01-2019','08-01-2019','Fujitsu POS',13,10,'Tested version 11.0 of pos',01,'Home')
 
 
 insert into Payroll (startDate,endDate,isApproved,isSubmitted,userId) values
